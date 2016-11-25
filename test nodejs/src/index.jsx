b017f0dff819b0abf69b6d18b5c31737a8833e7f
feuerman <valerymarkunin@gmail.com>
2016-11-25 16:08:34 +0300

    update test nodejs

--- test nodejs/src/index.jsx
+++ test nodejs/src/index.jsx
@@ -3,26 +3,9 @@ import ReactDOM from 'react-dom';
 
 import App from './app';
 
-<<<<<<< HEAD
 let appContainer = document.getElementById('app');
 
 ReactDOM.render(
 	<App />,
 	appContainer
 );
-=======
-const appContainer = document.getElementById('app');
-
-const data = {
-    name: 'Valery',
-    lastname: 'Markunin',
-    age: '28',
-    gender: 'male'
-};
-
-ReactDOM.render(
-    <App data={data}/>,
-    appContainer
-);
-
->>>>>>> c619bfdfc9cf7d75f75d803a26e24d7c04338798
